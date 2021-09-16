a68981a445b466fc95aabf158a1c6e7fca43d963
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-09-16 16:35:55 +0200

    Added some java modules

--- alchemy-defense/src/main/java/module-info.java
+++ alchemy-defense/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module alchemydefense {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.datatransfer;
+    requires java.desktop;
 
     opens alchemydefense to javafx.fxml;
     exports alchemydefense;
