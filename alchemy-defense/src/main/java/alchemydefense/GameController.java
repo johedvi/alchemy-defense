43da4e42ea77b19da76da0122c57120fbbb0b28b
Felix <felix.jonsson1993@gmail.com>
2021-09-17 11:31:54 +0200

    Add foundation for FXML (not solid)

--- alchemy-defense/src/main/java/alchemydefense/GameController.java
+++ alchemy-defense/src/main/java/alchemydefense/GameController.java
@@ -13,11 +13,24 @@ import java.io.IOException;
 
 public class GameController {
     @FXML
-    GridPane grid;
-    @FXML
-    ImageView controlPanelImage;
+    private GridPane grid;
 
-    public GameController(){
+
+    public void initialize(){/*
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("game.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        */
+        int col = grid.getColumnCount();
+        int row = grid.getRowCount();
+        System.out.println(col + " " + row);
+
     }
 
 }
