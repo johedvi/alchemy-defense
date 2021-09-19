8175d4cf0159b818d9691067bf94ca455db8cc84
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-09-19 22:11:57 +0200

    Updated creation of towers so it uses TowerFactory

--- alchemy-defense/src/main/java/alchemydefense/Model/GameModel.java
+++ alchemy-defense/src/main/java/alchemydefense/Model/GameModel.java
@@ -3,12 +3,22 @@ package alchemydefense.Model;
 import alchemydefense.Model.Board.ConcreteBoard;
 import alchemydefense.Model.Interfaces.Board;
 import alchemydefense.Model.Interfaces.BoardObject;
-import alchemydefense.Model.Towers.RedTower;
+import alchemydefense.Model.Towers.*;
 import alchemydefense.Model.Towers.Tower;
+import java.io.FileNotFoundException;
 
 
 import java.awt.*;
 
+/**
+ *
+ *
+ *
+ *
+ *----- Modified -----
+ * Date 09-19, By: Willem; Changed tower creation methods. Now towers no longer know their position.
+ *
+ */
 public class GameModel {
     Board concreteBoard;
 
@@ -17,8 +27,13 @@ public class GameModel {
     }
 
     public void placeTowerInCell(Tower.TowerType towerType, Point point) {
-        Tower tower = createTower();
-        concreteBoard.placeBoardObject(tower, point);
+        try {
+            Tower tower = createTower(towerType, point);
+            concreteBoard.placeBoardObject(tower, point);
+        }
+        catch (Exception e){
+            System.out.println("Not able to create the tower mentioned. Error: " + e.getMessage());
+        }
     }
 
     public BoardObject getBoardObjectInCell(Point point){
@@ -29,8 +44,7 @@ public class GameModel {
         concreteBoard.removeBoardObject(point);
     }
 
-    private Tower createTower(){
-        //TODO
-        return null;
+    private Tower createTower(Tower.TowerType towerType, Point point) throws IllegalArgumentException, FileNotFoundException {
+        return TowerFactory.createTower(towerType);
     }
 }
--- alchemy-defense/src/main/java/alchemydefense/Model/Towers/TowerFactory.java
+++ alchemy-defense/src/main/java/alchemydefense/Model/Towers/TowerFactory.java
@@ -2,23 +2,20 @@ package alchemydefense.Model.Towers;
 
 
 import java.awt.*;
+import java.lang.IllegalArgumentException;
 import java.io.FileNotFoundException;
-
 
 public class TowerFactory {
 
 
-    public static Tower createTower(Tower.TowerType towerType, Point CellPosition) throws FileNotFoundException {
+    public static Tower createTower(Tower.TowerType towerType) throws IllegalArgumentException, FileNotFoundException {
         if (towerType == Tower.TowerType.RED) {
-            return new RedTower(CellPosition);
+            return new RedTower();
         }
         else {
-            return null; //TODO
+            throw new IllegalArgumentException();
         }
     }
-
-
-
 }
 
 
