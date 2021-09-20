3b9cc5642c6aac18684c189baa272cb3995b11a5
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-09-20 17:03:07 +0200

    Made the module export everything

--- alchemy-defense/src/main/java/module-info.java
+++ alchemy-defense/src/main/java/module-info.java
@@ -7,4 +7,10 @@ module alchemydefense {
     opens alchemydefense to javafx.fxml;
     exports alchemydefense;
     exports alchemydefense.Model;
+    exports alchemydefense.Model.Board.Grid;
+    exports alchemydefense.Model.Foe;
+    exports alchemydefense.Model.Foe.Pathfinding;
+    exports alchemydefense.Model.Towers;
+    exports alchemydefense.Controller;
+    exports alchemydefense.Model.Interfaces;
 }
