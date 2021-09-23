9a77131e4960ee3ced4b3d5976a1af738dabaa52
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-09-23 17:25:12 +0200

    Fixed typo in healthAmountChanged

--- alchemy-defense/src/main/java/alchemydefense/Model/Player/PlayerEventListener.java
+++ alchemy-defense/src/main/java/alchemydefense/Model/Player/PlayerEventListener.java
@@ -2,5 +2,5 @@ package alchemydefense.Model.Player;
 
 public interface PlayerEventListener {
     void goldAmountChanged(int newValue);
-    void heathAmountChanged(int newValue);
+    void healthAmountChanged(int newValue);
 }
