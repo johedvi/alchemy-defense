0bbeb098ee5252ec129e1e881044a49280bd9f1f
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-09-23 16:47:51 +0200

    Removed test classes

--- alchemy-defense/src/test/java/DamageTest.java
+++ alchemy-defense/src/test/java/DamageTest.java
@@ -1,24 +1,12 @@
 import alchemydefense.Model.Board.ConcreteBoard;
-import alchemydefense.Model.Board.Grid.PositionalCell;
-import alchemydefense.Model.Foe.ConcreteFoe;
-import alchemydefense.Model.Foe.TestFoe;
-import alchemydefense.Model.Interfaces.Board;
-import alchemydefense.Model.Interfaces.Foe;
-import alchemydefense.Model.Towers.TestTower;
 import alchemydefense.Model.Towers.Tower;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-
-import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 public class DamageTest {
 
     ConcreteBoard board;
     Tower tower;
-    TestFoe foe;
 
     //Placera ut ett torn range 3
     //Placera ut en fiende inom abs(3)
@@ -27,8 +15,6 @@ public class DamageTest {
     @BeforeEach
     public void setup(){
         board = new ConcreteBoard();
-        tower = new TestTower();
-        foe = new TestFoe();
     }
 
     @Test
