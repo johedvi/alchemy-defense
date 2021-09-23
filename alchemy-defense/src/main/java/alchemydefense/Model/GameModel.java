48f26679a75bab6689a462b3a3d1dd1bd329a326
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-09-23 16:49:56 +0200

    Implemented use of wave in model loop

--- alchemy-defense/src/main/java/alchemydefense/Model/GameModel.java
+++ alchemy-defense/src/main/java/alchemydefense/Model/GameModel.java
@@ -6,6 +6,7 @@ import alchemydefense.Model.Foe.Pathfinding.PathFinder;
 import alchemydefense.Model.Interfaces.Board;
 import alchemydefense.Model.Interfaces.BoardListener;
 import alchemydefense.Model.Interfaces.BoardObject;
+import alchemydefense.Model.Interfaces.Foe;
 import alchemydefense.Model.Towers.*;
 import alchemydefense.Model.Towers.Tower;
 import alchemydefense.Model.Wave.Wave;
@@ -14,10 +15,8 @@ import java.io.FileNotFoundException;
 
 import java.awt.*;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
 
 /**
  *
@@ -31,22 +30,22 @@ import java.util.Set;
 public class GameModel {
     ConcreteBoard concreteBoard;
 
-    private List<ConcreteFoe> activeFoes = new ArrayList<>();
+    private LinkedList<Foe> activeFoes = new LinkedList<>();
     private final Set<BoardListener> boardListeners = new HashSet<BoardListener>();
 
     //TODO: write update method. Moves all foes and makes them take damage, call on placeObjects() in the end
 
     public GameModel(){
-
+        startNewWave();
         concreteBoard = new ConcreteBoard();
-        //REMOVE, only here to test foe rendering
-        concreteBoard.addFoe(new Point(0,0));
     }
 
     public void modelUpdate() {
-        //TODO: Fix foes update
 
-        //concreteBoard.updateFoes();
+        if (isWaveOver())
+            startNewWave();
+        else
+            concreteBoard.addFoe(activeFoes.removeFirst());
         concreteBoard.moveFoes();
         updateBoardListeners();
     }
@@ -73,9 +72,9 @@ public class GameModel {
         return TowerFactory.createTower(towerType);
     }
 
-    public void startNewWave(int boardHeight, PathFinder pathFinder) throws FileNotFoundException {
+    public void startNewWave() {
         Wave wave = new Wave();
-        activeFoes = wave.createFoes(boardHeight, pathFinder);
+        activeFoes = wave.createFoes();
     }
 
     private boolean isWaveOver() { return activeFoes.isEmpty(); }
