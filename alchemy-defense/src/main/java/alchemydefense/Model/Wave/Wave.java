c422986f2adf32d45a9942a0ecf7c279d4c3fc7a
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-09-23 16:50:32 +0200

    Changed argument for foes.add()

--- alchemy-defense/src/main/java/alchemydefense/Model/Wave/Wave.java
+++ alchemy-defense/src/main/java/alchemydefense/Model/Wave/Wave.java
@@ -1,13 +1,12 @@
 package alchemydefense.Model.Wave;
 
 import alchemydefense.Model.Foe.ConcreteFoe;
+import alchemydefense.Model.Foe.FoeFactory;
 import alchemydefense.Model.Foe.Pathfinding.PathFinder;
+import alchemydefense.Model.Interfaces.Foe;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Valdemar Stenhammar
@@ -29,11 +28,11 @@ public class Wave {
     }
 
     //TODO using a preliminary model to calculate how many foes should be created each round.
-    public List<ConcreteFoe> createFoes(int boardHeight, PathFinder pathFinder) throws FileNotFoundException {
+    public LinkedList<Foe> createFoes() {
         int nFoes = (int) (FIRST_WAVE_FOE_AMOUNT * Math.pow(WAVE_DIFFICULTY_FACTOR, (waveCounter-1)));
-        List<ConcreteFoe> foes = new ArrayList<>();
+        LinkedList<Foe> foes = new LinkedList<>();
         for(int i = 0; i <= nFoes; i++) {
-            foes.add(new ConcreteFoe(boardHeight, pathFinder));
+            foes.add(FoeFactory.createFoe(FoeFactory.FoeTypes.CONCRETE_FOE));
         }
         return foes;
     }
