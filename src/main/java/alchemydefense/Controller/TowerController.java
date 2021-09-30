02f593a9c34b459ff4df1544c9862a32e00702f8
ValleSten <valdemar.stenhammar@hotmail.com>
2021-09-30 13:15:46 +0200

    made pressedTower a boolean in TowerController to remove dependency from BoardObject

--- src/main/java/alchemydefense/Controller/TowerController.java
+++ src/main/java/alchemydefense/Controller/TowerController.java
@@ -1,16 +1,16 @@
 package alchemydefense.Controller;
 
-import alchemydefense.Model.Board.BoardObject;
 import alchemydefense.Model.GameModel;
 import alchemydefense.Model.Towers.Tower;
 
 import java.awt.*;
 
 public class TowerController {
+
+    private boolean pressedTower = false;
+
     private final GameModel model;
     private Tower.TowerType activeTower = null;
-
-    private BoardObject pressedTower = null;
 
     public TowerController(GameModel model) {
         this.model = model;
@@ -48,15 +48,14 @@ public class TowerController {
     }
 
     public void cellPressed(int x, int y) {
-        BoardObject boardObject = model.getBoardObjectInCell(new Point(x, y));
-        if(boardObject instanceof Tower) {
-            pressedTower = boardObject;
+        if(model.getBoardObjectInCell(new Point(x, y)) instanceof Tower) {
+            pressedTower = true;
             System.out.println("Tower pressed");
         }
         else {
-            pressedTower = null;
+            pressedTower = false;
         }
     }
 
-    public boolean isTowerPressed() { return this.pressedTower != null; }
+    public boolean isTowerPressed() { return this.pressedTower; }
 }
