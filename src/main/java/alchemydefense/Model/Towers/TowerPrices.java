90f436149e3bcc6c43a577ac4ea26944ffa4ec16
ValleSten <valdemar.stenhammar@hotmail.com>
2021-09-30 14:26:52 +0200

    Created singleton class TowerPrices

---
+++ src/main/java/alchemydefense/Model/Towers/TowerPrices.java
@@ -0,0 +1,47 @@
+package alchemydefense.Model.Towers;
+
+public class TowerPrices {
+
+    private static TowerPrices instance = null;
+
+    private static final int redTowerBuyPrice = 50;
+    private static final int blueTowerBuyPrice = 100;
+    private static final int greenTowerBuyPrice = 200;
+    private static final int purpleTowerBuyPrice = 400;
+
+    private static final int redTowerSellPrice = 10;
+    private static final int blueTowerSellPrice = 20;
+    private static final int greenTowerSellPrice = 40;
+    private static final int purpleTowerSellPrice = 80;
+
+    private TowerPrices() {}
+
+    public TowerPrices getInstance() {
+        if(instance == null) {
+            instance = new TowerPrices();
+        }
+        return instance;
+    }
+
+    private int getBuyPrice(Tower.TowerType towerType) {
+        int price = 0;
+        switch (towerType) {
+            case RED : price = redTowerBuyPrice; break;
+            case BLUE: price = blueTowerBuyPrice; break;
+            case GREEN: price = greenTowerBuyPrice; break;
+            case PURPLE: price = purpleTowerBuyPrice; break;
+        }
+        return price;
+    }
+
+    private int getSellPrice(Tower.TowerType towerType) {
+        int price = 0;
+        switch (towerType) {
+            case RED : price = redTowerSellPrice; break;
+            case BLUE: price = blueTowerSellPrice; break;
+            case GREEN: price = greenTowerSellPrice; break;
+            case PURPLE: price = purpleTowerSellPrice; break;
+        }
+        return price;
+    }
+}
