1d0a111d8eed7e1416a33da892868cfb9c32331e
ValleSten <valdemar.stenhammar@hotmail.com>
2021-09-30 14:30:31 +0200

    Removed more logic moved to TowerPrices from TowerTransaction

--- src/main/java/alchemydefense/Model/Towers/TowerTransaction.java
+++ src/main/java/alchemydefense/Model/Towers/TowerTransaction.java
@@ -7,16 +7,6 @@ public class TowerTransaction {
     private static final Player player = Player.getPlayer();
     private static final TowerPrices towerPrices = TowerPrices.getInstance();
 
-    private static final int redTowerBuyPrice = 50;
-    private static final int blueTowerBuyPrice = 100;
-    private static final int greenTowerBuyPrice = 200;
-    private static final int purpleTowerBuyPrice = 400;
-
-    private static final int redTowerSellPrice = 10;
-    private static final int blueTowerSellPrice = 20;
-    private static final int greenTowerSellPrice = 40;
-    private static final int purpleTowerSellPrice = 80;
-
     public TowerTransaction() {
 
     }
