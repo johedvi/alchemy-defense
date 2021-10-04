f293c83b2a1ba518c5b90f4a5c48ac5b5c1c6fc7
Willem Brahmstaedt <willem.brahmstaedt@gmail.com>
2021-10-04 15:14:43 +0200

    Deleted PixelRatios.java

--- src/main/java/alchemydefense/Utility/PixelRatios.java
+++
@@ -1,6 +0,0 @@
-package alchemydefense.Utility;
-
-public class PixelRatios {
-    private final static int PIXEL_PER_UNIT = 64;
-    private final static int PIXEL_SCALE_FACTOR = 4; //All pictures currently in 16x16 (2021-09-20)
-}
