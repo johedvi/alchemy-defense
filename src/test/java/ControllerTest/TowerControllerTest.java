4f4ae0176e17cb332c4e56d9929a94bd9876550d
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-10-05 15:28:09 +0200

    Added tests for sellTower and cellPressed

--- src/test/java/ControllerTest/TowerControllerTest.java
+++ src/test/java/ControllerTest/TowerControllerTest.java
@@ -66,4 +66,23 @@ public class TowerControllerTest {
         towerController.sellTower();
         Assertions.assertNull(model.getBoardObjectInCell(new Vector(2,2)));
     }
+
+    @Test
+    public void testTowerPressed() {
+        model.placeTowerInCell(TowerType.RED, new Point(2,2));
+        Assertions.assertFalse(towerController.isTowerPressed());
+        towerController.cellPressed(new Point(2,2));
+        Assertions.assertTrue(towerController.isTowerPressed());
+        towerController.cellPressed(new Point(1,1));
+        Assertions.assertFalse(towerController.isTowerPressed());
+    }
+
+    @Test
+    public void testSellTower() {
+        model.placeTowerInCell(TowerType.RED, new Point(2,2));
+        Assertions.assertEquals(RedTower.class, model.getBoardObjectInCell(new Point(2,2)).getClass());
+        towerController.cellPressed(new Point(2,2));
+        towerController.sellTower();
+        Assertions.assertNull(model.getBoardObjectInCell(new Point(2,2)));
+    }
 }
