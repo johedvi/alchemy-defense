408c6f29160750251918e161f340de56fe26fa38
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-06 22:13:20 +0200

    Copied JavaDoc to Foe-package

--- src/main/java/alchemydefense/Model/Foe/ConcreteFoe.java
+++ src/main/java/alchemydefense/Model/Foe/ConcreteFoe.java
@@ -3,14 +3,19 @@ package alchemydefense.Model.Foe;
 import alchemydefense.Model.Board.BoardObject;
 
 /**
- * @author Willem Brahmstaedt
  *
  * Class representing a concrete foe. Implements Foe, BoardObject and Health.
  *
  * Date: 2021-09-14
  *
+ * @author Willem Brahmstaedt
+ *
+ * ----- Modified -----
+ * Date: 2021-09-19, By Willem; Removed @Override for methods originally from BoardObject
+ *
+ * Date: 2021-09-20, By Valdemar; Changed method takeDamage to void and added method boolean isAlive instead.
+ *
  */
-
 public class ConcreteFoe implements BoardObject, Foe {
     private final int MAX_HP = 100;
     private int currentHP;
@@ -23,9 +28,7 @@ public class ConcreteFoe implements BoardObject, Foe {
     /**
      * Calculates damage taken
      * @param damageCount Represents how much damage that shall be taken
-     * @return True if the foe survives, false otherwise
      */
-
     @Override
     public void takeDamage(int damageCount) { setCurrentHP(getCurrentHP() - damageCount); }
 
--- src/main/java/alchemydefense/Model/Foe/Health.java
+++ src/main/java/alchemydefense/Model/Foe/Health.java
@@ -1,13 +1,18 @@
 package alchemydefense.Model.Foe;
 
 /**
- * @author Willem Brahmstaedt
  * Interface for representing health.
+ *
+ * @author Willem Brahmstaedt
  *
  * Date: 2021-09-14
  */
-
 public interface Health {
+
+    /**
+     * Deals damage equal to damageCount.
+     * @param damageCount the damage that should be taken.
+     */
     void takeDamage(int damageCount);
 
     int getMaxHP();
