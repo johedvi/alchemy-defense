6015717e344177862d2d1d26f8e476dbded5afc7
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-06 15:04:12 +0200

    Updated JavaDoc to Vector2Int

--- src/main/java/alchemydefense/Utility/Vector2Int.java
+++ src/main/java/alchemydefense/Utility/Vector2Int.java
@@ -1,22 +1,35 @@
 package alchemydefense.Utility;
 
-public class Vector2Int{
+/**
+ * Class representing a two-dimensional vector.
+ *
+ * @author Felix Jönsson
+ */
+
+public class Vector2Int {
     private int x;
     private int y;
 
-    public static int distanceBetweenVectorPoints(Vector2Int vector1, Vector2Int vector2){
-        double distanceX = Math.pow(vector1.x - vector2.x, 2);
-        double distanceY = Math.pow(vector1.y - vector2.y, 2);
-        return (int)Math.sqrt(distanceX + distanceY);
-    }
-
+    /**
+     * Constructor that assigns the object an X- and Y-value.
+     * @param x the x value.
+     * @param y the y value.
+     */
     public Vector2Int(int x, int y){
         this.x = x;
         this.y = y;
     }
 
-    public String toString(){
-        return "(" + x +", " + y + ")";
+    /**
+     * Calculates the distance between two vectors.
+     * @param vector1 the first vector.
+     * @param vector2 the second vector.
+     * @return the distance between vector1 and vector2.
+     */
+    public static int distanceBetweenVectorPoints(Vector2Int vector1, Vector2Int vector2){
+        double distanceX = Math.pow(vector1.x - vector2.x, 2);
+        double distanceY = Math.pow(vector1.y - vector2.y, 2);
+        return (int)Math.sqrt(distanceX + distanceY);
     }
 
     public void setVector(int x, int y) {
@@ -35,6 +48,11 @@ public class Vector2Int{
             return ((Vector2Int) obj).x == this.x && ((Vector2Int) obj).y == this.y;
         else
             return false;
+    }
+
+    @Override
+    public String toString(){
+        return "(" + x +", " + y + ")";
     }
 
     public int getX() { return x; }
