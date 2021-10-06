bdaf6ac904e1e19756678c45d1d6b5f1e4e6f5ec
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-06 22:18:05 +0200

    Copied JavaDoc to Player-package

--- src/main/java/alchemydefense/Model/Player/PlayerEventListener.java
+++ src/main/java/alchemydefense/Model/Player/PlayerEventListener.java
@@ -1,6 +1,23 @@
 package alchemydefense.Model.Player;
 
+/**
+ * Interface for the player to notify a change in the players status.
+ *
+ * @author Valdemar Stenhammar
+ *
+ * Date: 2021-09-21
+ */
 public interface PlayerEventListener {
+
+    /**
+     * Notifies all PlayerEventListeners that the players current gold has been updated.
+     * @param newValue the players new gold value.
+     */
     void goldAmountChanged(int newValue);
+
+    /**
+     * Notifies all PlayerEventListeners that the players current health has been updated.
+     * @param newValue the players new health value.
+     */
     void healthAmountChanged(int newValue);
 }
