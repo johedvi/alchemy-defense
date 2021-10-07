fb3d68149cef058df8ea1712ab228d355bc0e880
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-10-07 21:27:44 +0200

    Added letterbox() and inner class SceneSizeChangeListener

--- src/main/java/alchemydefense/App.java
+++ src/main/java/alchemydefense/App.java
@@ -8,9 +8,12 @@ import alchemydefense.View.SelectedTowerView;
 import alchemydefense.View.UserInterfaceView;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 
@@ -30,7 +33,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+        Scene scene = new Scene(root);
         setupAppWindow(stage, scene);
 
         InformationView informationView = new InformationView(SCENE_WIDTH);
@@ -43,8 +46,12 @@ public class App extends Application {
         model.addBoardListener(view);
         model.addPlayerEventListener(informationView);
 
+        stage.setScene(scene);
+        //stage.minWidthProperty().bind(scene.heightProperty().multiply(2));
+        //stage.minHeightProperty().bind(scene.widthProperty().divide(2));
         stage.show();
         gameUpdate();
+        letterbox(scene, view);
     }
 
 
@@ -85,4 +92,51 @@ public class App extends Application {
                 launch();
         }
 
+    private void letterbox(final Scene scene, final BoardView contentPane) {
+        final double initWidth  = scene.getWidth();
+        final double initHeight = scene.getHeight();
+        final double ratio      = initWidth / initHeight;
+
+        SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane);
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
+
+    private static class SceneSizeChangeListener implements ChangeListener<Number> {
+        private final Scene scene;
+        private final double ratio;
+        private final double initHeight;
+        private final double initWidth;
+        private final BoardView contentPane;
+
+        public SceneSizeChangeListener(Scene scene, double ratio, double initHeight, double initWidth, BoardView contentPane) {
+            this.scene = scene;
+            this.ratio = ratio;
+            this.initHeight = initHeight;
+            this.initWidth = initWidth;
+            this.contentPane = contentPane;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+            final double newWidth  = scene.getWidth();
+            final double newHeight = scene.getHeight();
+
+            double scaleFactor =
+                    newWidth / newHeight > ratio
+                            ? newHeight / initHeight
+                            : newWidth / initWidth;
+
+            if (scaleFactor >= 1) {
+                Scale scale = new Scale(scaleFactor, scaleFactor);
+                scale.setPivotX(0);
+                scale.setPivotY(0);
+                scene.getRoot().getTransforms().setAll(scale);
+
+                contentPane.setSize(newWidth  / scaleFactor, newHeight / scaleFactor);
+            } else {
+                contentPane.setSize(Math.max(initWidth,  newWidth), Math.max(initHeight, newHeight));
+            }
+        }
+    }
 }
