3ee897996c2c3c9e167de52597fe853ef60c5b1e
Felix <felix.jonsson1993@gmail.com>
2021-10-10 12:37:59 +0200

    Add javaDocs for Pathfinder

--- src/main/java/alchemydefense/Model/Board/Pathfinding/Pathfinder.java
+++ src/main/java/alchemydefense/Model/Board/Pathfinding/Pathfinder.java
@@ -4,13 +4,18 @@ import java.util.*;
 import alchemydefense.Utility.Vector;
 
 /**
- * Repres
+ * Pathfinder class that can create paths when supplied a graph through a GraphManager to traverse. Uses breadth first search to search the graph
+ * and returns the shortest list by keeping track of the nodes with a linked list.
+ * @Author Felix Jönsson
  */
 public class Pathfinder {
     Vector[] directions = {Vector.right(), Vector.down(), Vector.left(), Vector.up()};
 
     Vector start;
     Vector goal;
+
+    Vector testStart;
+    Vector testGoal;
 
     PathNode startNode;
     PathNode destinationNode;
@@ -20,30 +25,44 @@ public class Pathfinder {
     Queue<PathNode> nodeFront = new LinkedList<>();
     HashMap<Vector, PathNode> pathGraph;
 
-    public Pathfinder(GraphManager graphManager){
-        pathGraph = graphManager.getGraph();
+
+    /**
+     * Creates a new instance and assigns a GraphManager to it. Also sets up a start and goal
+     * Vector for testing of possible path blocks.
+     * @param graphManager the GraphManager assigned to this instance
+     * @param testStart the start Vector used for testing possible path blocks.
+     * @param testGoal the goal Vector used for testing possible path blocks.
+     */
+    public Pathfinder(GraphManager graphManager, Vector testStart, Vector testGoal){
+        this.pathGraph = graphManager.getGraph();
+        this.testStart = testStart;
+        this.testGoal = testGoal;
     }
 
+    /**
+     * Creates and returns a new PathNode List using breadth first search. The breadth first search is set by default
+     * to explore the neighbour to the right first in each node iteration.
+     * @param start start position for the path to be generated
+     * @param end end position for the path to be generated
+     * @return list of PathNodes. Client can parse each PathNode coordinateVector to get a valid path to use.
+     */
     public List<PathNode> generateNewPath(Vector start, Vector end){
         setup(start, end);
         breadthFirstSearch();
         return buildPath();
     }
 
-    private void setup(Vector start, Vector end) {
-        this.start = start;
-        goal = end;
-        startNode = new PathNode(this.start);
-        destinationNode = new PathNode(goal);
-        pathGraph.put(destinationNode.getCoordinateVector(), destinationNode);
-        currentNode = startNode;
-    }
-
+    /**
+     * Sets the PathNode connected to the argument Vector temporarily to non-traversable. If this results in the path being
+     * unable to find a valid path to the end destination the method will return false, else true.
+     * @param coordinates coordinates of the PathNode to temporarily block
+     * @return boolean representing whether blocking the PathNode results in blocking the path
+     */
     public boolean blocksPath(Vector coordinates){
         if(pathGraph.containsKey(coordinates)){
             boolean previousNodeState = pathGraph.get(coordinates).isTraversable();
             pathGraph.get(coordinates).setTraversable(false);
-            List<PathNode> path = generateNewPath(new Vector(0,2), new Vector(11,2));
+            List<PathNode> path = generateNewPath(testStart, testGoal);
             pathGraph.get(coordinates).setTraversable(previousNodeState);
             if(path.size() <= 1){
                 return true;
@@ -55,9 +74,9 @@ public class Pathfinder {
     private void exploreAdjacentNodes(){
         List<PathNode> neighbors = new ArrayList<>();
         for (Vector direction : directions){
-            Vector neighborCoords = currentNode.getCoordinateVector().add(direction);
-            if(pathGraph.containsKey(neighborCoords)){
-                neighbors.add(pathGraph.get(neighborCoords));
+            Vector neighborCoordinate = currentNode.getCoordinateVector().add(direction);
+            if(pathGraph.containsKey(neighborCoordinate)){
+                neighbors.add(pathGraph.get(neighborCoordinate));
             }
         }
 
@@ -68,6 +87,16 @@ public class Pathfinder {
                 nodeFront.add(neighbor);
             }
         }
+    }
+
+
+    private void setup(Vector start, Vector end) {
+        this.start = start;
+        goal = end;
+        startNode = new PathNode(this.start);
+        destinationNode = new PathNode(goal);
+        pathGraph.put(destinationNode.getCoordinateVector(), destinationNode);
+        currentNode = startNode;
     }
 
     private void breadthFirstSearch(){
