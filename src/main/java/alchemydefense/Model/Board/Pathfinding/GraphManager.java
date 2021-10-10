f5c2d6ca4d9fe4cc79ae98c5e8c3f45ef86bffba
Felix <felix.jonsson1993@gmail.com>
2021-10-10 12:45:23 +0200

    Add javaDocs for GraphManager <(o.O<)

--- src/main/java/alchemydefense/Model/Board/Pathfinding/GraphManager.java
+++ src/main/java/alchemydefense/Model/Board/Pathfinding/GraphManager.java
@@ -4,14 +4,23 @@ import alchemydefense.Utility.Vector;
 
 import java.util.HashMap;
 
-// NEEDS DOCS, DONT COMMENT ON THIS IF YOU ARE REVIEWING :)))))
+/**
+ * Sets up a graph of PathNodes connected to a specific Vector through a HasMap. Offers graph management
+ * through various methods.
+ * @author Felix Jönsson
+ */
 public class GraphManager {
-    HashMap<Vector, PathNode> graph = new HashMap<>();
+    private HashMap<Vector, PathNode> graph = new HashMap<>();
 
     public HashMap<Vector, PathNode> getGraph() {
         return graph;
     }
 
+    /**
+     * Creates a new instance of a GraphManager and assigns the dimensions of the graph.
+     * @param width width dimension
+     * @param height height dimension
+     */
     public GraphManager(int width, int height){
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
@@ -21,12 +30,20 @@ public class GraphManager {
         }
     }
 
+    /**
+     * Blocks the PathNode connected to the argument Vector.
+     * @param coordinates PathNode to block
+     */
     public void blockPathNode(Vector coordinates){
         if(graph.containsKey(coordinates)){
             graph.get(coordinates).setTraversable(false);
         }
     }
 
+    /**
+     * Unblocks the PathNode connected to the argument Vector.
+     * @param coordinates PathNode to unblock
+     */
     public void unblockPathNode(Vector coordinates){
         if(graph.containsKey(coordinates)){
             graph.get(coordinates).setTraversable(true);
