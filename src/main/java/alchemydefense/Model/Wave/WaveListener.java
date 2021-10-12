c50526d8ab4c598199ceb0930793e3c759aa66af
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-12 10:37:50 +0200

    Added JavaDoc to WaveListener

--- src/main/java/alchemydefense/Model/Wave/WaveListener.java
+++ src/main/java/alchemydefense/Model/Wave/WaveListener.java
@@ -1,6 +1,17 @@
 package alchemydefense.Model.Wave;
 
+/**
+ * Interface to notify when a new wave is started.
+ *
+ * @author Valdemar Stenhammar
+ *
+ * Date: 2021-10-12
+ */
 public interface WaveListener {
 
+    /**
+     * Notifies all WaveListeners that the wave counter has been updated.
+     * @param newWaveValue the new value of waveCounter.
+     */
     void waveCounterChanged(int newWaveValue);
 }
