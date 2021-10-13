e803cf7e021d4d24bfc1804268949542fa339ec4
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-13 12:58:55 +0200

    Override toString method in BoardObjectType

--- src/main/java/alchemydefense/Utility/BoardObjectType.java
+++ src/main/java/alchemydefense/Utility/BoardObjectType.java
@@ -1,7 +1,7 @@
 package alchemydefense.Utility;
 
 /**
- * Enum representing different tower types.
+ * Enum representing different board object types.
  *
  * @author Johan Linden
  *
@@ -10,5 +10,19 @@ package alchemydefense.Utility;
 public enum BoardObjectType {
     RED_TOWER, BLUE_TOWER, GREEN_TOWER, PURPLE_TOWER,
 
-    CONCRETE_FOE
+    CONCRETE_FOE;
+
+    @Override
+    public String toString() {
+        String string;
+        switch (this) {
+            case RED_TOWER: string = "Red Tower"; break;
+            case BLUE_TOWER: string = "Blue Tower"; break;
+            case GREEN_TOWER: string = "Green Tower"; break;
+            case PURPLE_TOWER: string = "Purple Tower"; break;
+            case CONCRETE_FOE: string = "Concrete Foe"; break;
+            default: string = "BoardObjectType not found";
+        }
+        return string;
+    }
 }
