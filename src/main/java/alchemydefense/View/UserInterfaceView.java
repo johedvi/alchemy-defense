cb029e257c9aab50b00dcc74d3a9c8c86aca1a9a
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-15 13:50:54 +0200

    Changed order of tower buttons to be sorted from lowest to highest in price.

--- src/main/java/alchemydefense/View/UserInterfaceView.java
+++ src/main/java/alchemydefense/View/UserInterfaceView.java
@@ -42,7 +42,7 @@ public class UserInterfaceView extends Pane {
         redTowerButton.setMaxSize(100, 200);
         redTowerButton.setPrefHeight(50);
         redTowerButton.setPrefWidth(100);
-        redTowerButton.setLayoutX(scene_width / 2 - 280);
+        redTowerButton.setLayoutX(scene_width / 2 - 400);
         redTowerButton.setLayoutY(unit_in_pixel - 3 * unit_in_pixel / 4);
         redTowerButton.setContentDisplay(ContentDisplay.TOP);
         redTowerButton.setOnMouseClicked(e -> {
@@ -55,7 +55,7 @@ public class UserInterfaceView extends Pane {
         blueTowerButton.setMaxSize(100, 200);
         blueTowerButton.setPrefHeight(50);
         blueTowerButton.setPrefWidth(100);
-        blueTowerButton.setLayoutX(scene_width / 2 - 400);
+        blueTowerButton.setLayoutX(scene_width / 2 - 280);
         blueTowerButton.setLayoutY(unit_in_pixel - 3 * unit_in_pixel / 4);
         blueTowerButton.setContentDisplay(ContentDisplay.TOP);
         blueTowerButton.setOnMouseClicked(e -> {
@@ -64,25 +64,11 @@ public class UserInterfaceView extends Pane {
         blueTowerButton.setGraphic(blueCrystal);
         this.getChildren().add(blueTowerButton);
 
-        javafx.scene.control.Button purpleTowerButton = new javafx.scene.control.Button("Purple Tower");
-        purpleTowerButton.setMaxSize(100, 200);
-        purpleTowerButton.setPrefHeight(50);
-        purpleTowerButton.setPrefWidth(100);
-        purpleTowerButton.setLayoutX(scene_width / 2 - 160);
-        purpleTowerButton.setLayoutY(unit_in_pixel - 3 * unit_in_pixel / 4);
-
-        purpleTowerButton.setContentDisplay(ContentDisplay.TOP);
-        purpleTowerButton.setOnMouseClicked(e -> {
-            towerController.setPurpleTowerActive();
-        });
-        purpleTowerButton.setGraphic(purpleCrystal);
-        this.getChildren().add(purpleTowerButton);
-
         javafx.scene.control.Button greenTowerButton = new javafx.scene.control.Button("Green Tower");
         greenTowerButton.setMaxSize(100, 200);
         greenTowerButton.setPrefHeight(50);
         greenTowerButton.setPrefWidth(100);
-        greenTowerButton.setLayoutX(scene_width / 2 - 40);
+        greenTowerButton.setLayoutX(scene_width / 2 - 160);
         greenTowerButton.setLayoutY(unit_in_pixel - 3 * unit_in_pixel / 4);
         greenTowerButton.setContentDisplay(ContentDisplay.TOP);
         greenTowerButton.setOnMouseClicked(e -> {
@@ -90,6 +76,19 @@ public class UserInterfaceView extends Pane {
         });
         greenTowerButton.setGraphic(greenCrystal);
         this.getChildren().add(greenTowerButton);
+
+        javafx.scene.control.Button purpleTowerButton = new javafx.scene.control.Button("Purple Tower");
+        purpleTowerButton.setMaxSize(100, 200);
+        purpleTowerButton.setPrefHeight(50);
+        purpleTowerButton.setPrefWidth(100);
+        purpleTowerButton.setLayoutX(scene_width / 2 - 40);
+        purpleTowerButton.setLayoutY(unit_in_pixel - 3 * unit_in_pixel / 4);
+        purpleTowerButton.setContentDisplay(ContentDisplay.TOP);
+        purpleTowerButton.setOnMouseClicked(e -> {
+            towerController.setPurpleTowerActive();
+        });
+        purpleTowerButton.setGraphic(purpleCrystal);
+        this.getChildren().add(purpleTowerButton);
 
         javafx.scene.control.Button newWaveButton = new javafx.scene.control.Button("Start new wave");
         newWaveButton.setMaxSize(100, 200);
