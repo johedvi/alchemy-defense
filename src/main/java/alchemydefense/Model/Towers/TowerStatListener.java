15454e788a6616ba4b6a0afc880161061fceb0aa
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-15 13:46:08 +0200

    Added JavaDoc to TowerStatListener

--- src/main/java/alchemydefense/Model/Towers/TowerStatListener.java
+++ src/main/java/alchemydefense/Model/Towers/TowerStatListener.java
@@ -1,7 +1,21 @@
 package alchemydefense.Model.Towers;
 
+/**
+ * Interface for delivering a towers stats.
+ *
+ * @author Valdemar Stenhammar
+ *
+ * Date: 2021-10-13.
+ */
 public interface TowerStatListener {
 
+    /**
+     * Displays the towers stats.
+     * @param towerType the type of tower.
+     * @param range the range of the tower.
+     * @param damage the damage of the tower.
+     * @param sellPrice the sell price of the tower.
+     */
     void displayTowerStats(String towerType, int range, int damage, int sellPrice);
 
 }
