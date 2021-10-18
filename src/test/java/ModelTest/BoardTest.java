d93e7797b699acf7e757edf3d00d5f4ba45e3460
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-10-18 15:20:41 +0200

    Created class for test of Board

---
+++ src/test/java/ModelTest/BoardTest.java
@@ -0,0 +1,16 @@
+package ModelTest;
+
+import alchemydefense.Model.Board.Board;
+import alchemydefense.Model.Board.ConcreteBoard;
+import alchemydefense.Model.GameModel;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    static Board board;
+
+    @BeforeAll
+    public static void beforeAll() {
+    }
+
+}
