965c7be52c574fcfdb39e7e87a7aa405fedd95c9
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-10-18 14:28:05 +0200

    Added tests for Tile.java

--- src/test/java/ModelTest/GridTest.java
+++ src/test/java/ModelTest/GridTest.java
@@ -1,5 +1,8 @@
 package ModelTest;
 
+import alchemydefense.Model.Board.ConcreteBoard;
+import alchemydefense.Model.Board.Grid.Tile;
+import alchemydefense.Model.Foe.ConcreteFoe;
 import alchemydefense.Model.GameModel;
 import alchemydefense.Model.Towers.TowerHierarchy.RedTower;
 import alchemydefense.Utility.BoardObjectType;
@@ -11,11 +14,13 @@ import alchemydefense.Utility.Vector;
 
 public class GridTest {
     private static GameModel gameModel;
+    private static ConcreteBoard board;
     Vector testPoint;
 
     @BeforeAll
     public static void setGameModel(){
         gameModel = new GameModel();
+        board = new ConcreteBoard();
     }
 
     @Test
@@ -32,5 +37,38 @@ public class GridTest {
         Assertions.assertEquals(gameModel.getBoardObjectInCell(testPoint).getClass(), RedTower.class);
         gameModel.removeBoardObjectInCell(testPoint);
         Assertions.assertNull(gameModel.getBoardObjectInCell(testPoint));
+    }
+    @Test
+    public void testCellsInRange() {
+        Assertions.assertEquals(0,board.getCell(new Vector(2,2)).getPositionalCellsWithinRange(board).size());
+        board.placeTower(new RedTower(), new Vector(2,2));
+        Assertions.assertEquals(21,board.getCell(new Vector(2,2)).getPositionalCellsWithinRange(board).size());
+    }
+
+    @Test
+    public void testFoeInCell() {
+        Tile tile = new Tile(2,2);
+
+        Assertions.assertFalse(tile.hasFoe());
+        tile.addFoe(new ConcreteFoe());
+        Assertions.assertTrue(tile.hasFoe());
+        tile.removeFoe();
+        Assertions.assertFalse(tile.hasFoe());
+    }
+
+    @Test
+    public void testToString() {
+        Tile tile = new Tile(2,2);
+
+        Assertions.assertEquals("(2, 2)", tile.toString());
+    }
+
+    @Test
+    public void testUpdateFlag() {
+        Tile tile = new Tile(2,2);
+
+        Assertions.assertFalse(tile.isUpdated());
+        tile.setUpdated(true);
+        Assertions.assertTrue(tile.isUpdated());
     }
 }
