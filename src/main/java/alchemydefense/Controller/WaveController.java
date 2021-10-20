bce72d28554206c54b2450250c50febf076faf9f
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-10-20 15:21:46 +0200

    Created a class for a Wave controller

---
+++ src/main/java/alchemydefense/Controller/WaveController.java
@@ -0,0 +1,21 @@
+package alchemydefense.Controller;
+
+import alchemydefense.Model.IWaveHandler;
+
+
+/**
+ * Controller used for starting a new wave
+ *
+ * @author Willem Brahmstaedt
+ */
+public class WaveController implements IWaveController {
+    private final IWaveHandler waveHandler;
+
+    public WaveController(IWaveHandler waveHandler) { this.waveHandler = waveHandler; }
+
+    /**
+     * Delegates the request of a new wave to the IWaveHandler
+     */
+    @Override
+    public void startNewWave() { waveHandler.startNewWave(); }
+}
