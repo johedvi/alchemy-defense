eef5e08aeeb5ad3ae06fc3f27516ac3335ccf90a
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2021-10-20 15:20:57 +0200

    Created an interface representing the functionality of a Wave handler

---
+++ src/main/java/alchemydefense/Model/IWaveHandler.java
@@ -0,0 +1,13 @@
+package alchemydefense.Model;
+
+/**
+ * Interface representing the functionality of starting a new wave
+ *
+ * @author Willem Brahmstaedt
+ */
+public interface IWaveHandler {
+    /**
+     * Starts a new wave
+     */
+    void startNewWave();
+}
