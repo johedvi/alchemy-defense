a8008b2fa08a61a35446a50e8b412d1206b46500
ValleSten <valdemar.stenhammar@hotmail.com>
2021-10-24 14:14:44 +0200

    Changed price and damage of red tower.

--- src/main/java/alchemydefense/Model/Towers/TowerFactory.java
+++ src/main/java/alchemydefense/Model/Towers/TowerFactory.java
@@ -20,7 +20,7 @@ public class TowerFactory {
     public static ITower createTower(BoardObjectType boardObjectType)  {
 
         switch (boardObjectType) {
-            case RED_TOWER: return new Tower(BoardObjectType.RED_TOWER, "red-crystal.png",new AttackDamageSystem(2,20), new PriceSystem(0,10));
+            case RED_TOWER: return new Tower(BoardObjectType.RED_TOWER, "red-crystal.png",new AttackDamageSystem(2,10), new PriceSystem(50,10));
             case BLUE_TOWER: return new Tower(BoardObjectType.BLUE_TOWER, "blue-crystal.png", new AttackDamageSystem(2,20), new PriceSystem(100,20));
             case GREEN_TOWER: return new Tower(BoardObjectType.GREEN_TOWER,"green-crystal.png",  new AttackDamageSystem(2,20), new PriceSystem(200,40));
             case PURPLE_TOWER: return new Tower(BoardObjectType.PURPLE_TOWER,"purple-crystal.png",  new AttackDamageSystem(3,30), new PriceSystem(400,80));
